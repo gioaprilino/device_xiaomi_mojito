ce0f72fb46c9a4ef53789addb09c8c776f860916
Jens Reidel <adrian@travitia.xyz>
2023-08-28 10:42:51 +0545

    mojito: Explicitly disable serial console While it should already be disabled, the system searches for 'console=null' in cmdline to make sure that the serial console is disabled. Add this part to make the permanent notification disappear.
    Signed-off-by: romgharti <romgharti@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,6 +129,7 @@ BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=qcom
 BOARD_KERNEL_CMDLINE += androidboot.memcg=1
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE += console=null
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1
