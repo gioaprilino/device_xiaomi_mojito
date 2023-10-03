5c8fc6cec5ca0b833b87ec98b9a51839c43af591
Gio Aprilino <gioaprilino96@gmail.com>
2023-10-03 08:19:10 +0700

    mojito: AICPify

--- aicp_mojito.mk
+++ aicp_mojito.mk
@@ -7,6 +7,8 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_r.mk)
 
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
@@ -24,7 +26,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 # Some Build Flags
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USES_BLUR := false
+TARGER_GAPPS_ARCH := arm64
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
